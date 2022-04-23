f84bb9da1461d1c310c76024139bea3e55d4f0cb
kasiasolinska <catherine.bus@gmail.com>
2022-04-23 11:03:57 +0200

    buttons + -

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.scss']
 })
 export class CounterComponent implements OnInit {
+  public value = 123; 
+  public text = 'ala ma kota';
+
+  onClick () {
+    this.text = 'asd';
+
+  }
+
+increment() {
+  this.value++;
+}
+
+decrement() {
+  this.value--;
+}
 
   constructor() { }
 
