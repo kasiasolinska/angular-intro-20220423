59e43d2c884ece7f697d653087bd5507fd7ebeff
kasiasolinska <catherine.bus@gmail.com>
2022-04-24 12:39:37 +0200

    remove all todos

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -43,6 +43,11 @@ export class TodosComponent implements OnInit {
 
   }
 
+
+  public removeAll(): void{
+this.todos = [];
+this.storageService.saveTodos(this.todos);
+  }
   constructor(
     private storageService: StorageService) { 
 
