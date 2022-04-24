c32bd2c081bec4f1958033f649a91fb5287ee1fe
kasiasolinska <catherine.bus@gmail.com>
2022-04-24 14:56:44 +0200

    reverse first name pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { TimerComponent } from './timer/timer.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HighlightDirective } from './highlight.directive';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, TodosComponent, TodoComponent, TimerComponent, UsersComponent, HighlightDirective],
+  declarations: [AppComponent, CounterComponent, TodosComponent, TodoComponent, TimerComponent, UsersComponent, HighlightDirective, ReversePipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,],
   providers: [],
   bootstrap: [AppComponent],
